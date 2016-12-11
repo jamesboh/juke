902136e33f2ca2661aed51951cff9d977ee06c28
James Oh <jamesboh@gmail.com>
2016-12-10 19:22:09 -0500

    stateless component with destructured assignment in function definition

--- browser/react/components/album.js
+++ browser/react/components/album.js
@@ -2,25 +2,19 @@
 
 import React, { Component } from 'react';
 
-class Album extends Component {
-	render() {
-		const { album, handleClick } = this.props;
-
-		return (
-			<div className="col-xs-4">
-		      <a className="thumbnail" href="#" onClick={ () => handleClick(album) }>
-		        <img src={album.imageUrl} />
-		        <div className="caption">
-		          <h5>
-		            <span>{album.name}</span>
-		          </h5>
-		          <small>{album.songs.length} songs</small>
-		        </div>
-		      </a>
-		    </div>
-		)
-	}
-}
+const Album = ({ album, handleClick }) => (
+	<div className="col-xs-4">
+      <a className="thumbnail" href="#" onClick={ () => handleClick(album) }>
+        <img src={album.imageUrl} />
+        <div className="caption">
+          <h5>
+            <span>{album.name}</span>
+          </h5>
+          <small>{album.songs.length} songs</small>
+        </div>
+      </a>
+    </div>
+)
 
 export default Album;
 
